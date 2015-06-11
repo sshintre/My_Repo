53b3463378265b3f9600cc8e714a1fae623ea8bd
sshintre <sagarshintre03@rediffmail.com>
2015-06-11 12:32:23 +0530

    commited to branch1

--- Print/src/Print.java
+++ Print/src/Print.java
@@ -3,7 +3,8 @@ public class Print {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("in my city");
+        System.out.println("in my city");
+        System.out.println("in branch1");   
 	}
 
 }
